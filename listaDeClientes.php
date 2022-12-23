7341578302ba57cefe21f684197aac986b0050ff
vinicius.sibin <vinisibim@gmail.com>
2022-12-23 09:29:58 -0300

    Criado tabela para listar os clientes

---
+++ listaDeClientes.php
@@ -0,0 +1,41 @@
+<?php
+    require_once("conexão.php");
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de clientes</title>
+</head>
+<body>
+    <h1>Lista de clientes</h1>
+    <p>Clientes cadastrados no sistema</p>
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Telefone</th>
+            <th>Data Nascimento</th>
+            <th>Data Cadastro</th>
+            <th></th>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
