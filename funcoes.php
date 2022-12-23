9618c2d1b2072f0e1164791fe5e3c5db2c285fe3
vinicius.sibin <vinisibim@gmail.com>
2022-12-23 09:56:12 -0300

    Criado função para transformar a data de nascimento do banco para visulização em pt-br

--- funcoes.php
+++ funcoes.php
@@ -2,4 +2,8 @@
     function limpar_texto($str){
         return preg_replace("/[^0-9]/", "", $str);
     }
+
+    function visualizaNascimento($str){
+        return implode('/', array_reverse(explode('-', $str)));
+    }
 ?>
