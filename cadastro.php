d0d7db44a273daf1768c96cf92afefc631f7e8d0
vinicius.sibin <vinisibim@gmail.com>
2022-12-26 12:57:19 -0300

    colocado para a mensagem de erro aparecer dentro do formulário

--- cadastro.php
+++ cadastro.php
@@ -1,71 +1,6 @@
 <?php
     require_once("conexao.php");
-    require_once("funcoes.php");
-
-    if(count($_POST)>0){
-        $erro = false;
-
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $telefone = $_POST['telefone'];
-        $nascimento = $_POST['nascimento'];
-        $senha = $_POST['senha'];
-        $confirmaSenha = $_POST['confirmaSenha'];
-
-
-        if(empty($nome)){
-            $erro = "Preencha o campo nome!";
-        } else if((strlen($nome)<6) || (strlen($nome)>60)){
-            $erro = "O nome deve ter entre 6 60 caracteres!";
-        } else if(substr($nome, 0, 1) == " "){
-            $erro = "O nome não deve iniciar com espaço";
-        }
-
-        if(empty($email)){
-            $erro = "Preencha o campo E-mail!";
-        } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $erro = "O E-mail deve ser preenchido no padrão: exemplo@gmail.com";
-        }
-
-        if(!empty($telefone)){
-            $telefone = limpar_texto($telefone);
-            if(strlen($telefone) != 11){
-                $erro = "O telefone foi preenchido incorretamente!";
-            }
-        }
-
-        if(empty($nascimento)){
-            $erro = "Preencha o campo de data de nascimento!";
-        } else if (strlen($nascimento = implode('-', array_reverse(explode('/',$nascimento)))) != 10){
-            $erro = "Data de Nascimento está incorreta!";
-        }
-
-        if(empty($senha)){
-            $erro = "Preencha o campo Senha";
-        } else if(strlen($senha)<6 || strlen($senha)>20){
-            $erro = "A senha deve conter entre 6 e 20 caracteres.";
-        } else if(substr($senha, 0, 1) == " "){
-            $erro = "A senha não deve iniciar com espaço";
-        }
-
-        if($confirmaSenha != $senha){
-            $erro = "A confirmação da senha deve ser igual a senha digitada!";
-        }
-
-        if($erro){
-            echo "<p><b>ERRO: $erro</b></p>";
-        } else {
-            $senha = password_hash($senha, PASSWORD_DEFAULT);
-
-            $sql = "INSERT INTO clientes (nome, email, senha, telefone, nascimento, dataCadastro) VALUES ('$nome', '$email', '$senha', '$telefone', '$nascimento', NOW())";
-            $sqlQuery = $mysqli->query($sql) or die($mysqli->error);
-
-            $mensagem = "<h1>Cadastro do usuário: $nome realizado com sucesso!!!</h1><p>Para realizar o login no sistema acesse <b>http://localhost/CURSO-PHP/</b></p><p>Usuário: $email <br> Senha: ...</p>";
-            enviarEmail("vinisibim@gmail.com", "Cadastro de Usuário no sistema", $mensagem);
-
-            echo "Usuário cadastrado com sucesso!!";
-        }
-    }
+    require_once("funcoes.php");    
 ?>
 
 <!DOCTYPE html>
@@ -104,9 +39,79 @@
             <label>Confirmar Senha:</label>
             <input value="<?php if(isset($_POST['confirmaSenha'])) echo $_POST['confirmaSenha'] ?>" type="password" name="confirmaSenha" placeholder="Ex: @1234Senha"> *
         </p>
+       
+        <?php 
+
+if(count($_POST)>0){
+    $erro = false;
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $nascimento = $_POST['nascimento'];
+    $senha = $_POST['senha'];
+    $confirmaSenha = $_POST['confirmaSenha'];
+
+
+    if(empty($nome)){
+        $erro = "Preencha o campo nome!";
+    } else if((strlen($nome)<6) || (strlen($nome)>60)){
+        $erro = "O nome deve ter entre 6 e 60 caracteres!";
+    } else if(substr($nome, 0, 1) == " "){
+        $erro = "O nome não deve iniciar com espaço";
+    }
+
+    if(empty($email)){
+        $erro = "Preencha o campo E-mail!";
+    } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $erro = "O E-mail deve ser preenchido no padrão: exemplo@gmail.com";
+    }
+
+    if(!empty($telefone)){
+        $telefone = limpar_texto($telefone);
+        if(strlen($telefone) != 11){
+            $erro = "O telefone foi preenchido incorretamente!";
+        }
+    }
+
+    if(empty($nascimento)){
+        $erro = "Preencha o campo de data de nascimento!";
+    } else if (strlen($nascimento = implode('-', array_reverse(explode('/',$nascimento)))) != 10){
+        $erro = "Data de Nascimento está incorreta!";
+    }
+
+    if(empty($senha)){
+        $erro = "Preencha o campo Senha";
+    } else if(strlen($senha)<6 || strlen($senha)>20){
+        $erro = "A senha deve conter entre 6 e 20 caracteres.";
+    } else if(substr($senha, 0, 1) == " "){
+        $erro = "A senha não deve iniciar com espaço";
+    }
+
+    if($confirmaSenha != $senha){
+        $erro = "A confirmação da senha deve ser igual a senha digitada!";
+    }
+            if($erro){
+                echo "<p style='color: red;'><b>ERRO: $erro</b></p>";
+            } else {
+                $senha = password_hash($senha, PASSWORD_DEFAULT);
+                
+                $sql = "INSERT INTO clientes (nome, email, senha, telefone, nascimento, dataCadastro) VALUES ('$nome', '$email', '$senha', '$telefone', '$nascimento', NOW())";
+                $sqlQuery = $mysqli->query($sql) or die($mysqli->error);
+    
+                $mensagem = "<h1>Cadastro do usuário: $nome realizado com sucesso!!!</h1><p>Para realizar o login no sistema acesse <b>http://localhost/CURSO-PHP/</b></p><p>Usuário: $email <br> Senha: $confirmaSenha</p>";
+                $email = 'vinisibim@gmail.com';
+                enviarEmail($email, "Cadastro de Usuário no sistema", $mensagem);
+    
+                echo "<p style='color: green;'><b>Usuário cadastrado com sucesso!!</b></p>";
+            }
+
+        }
+        ?>
         <p>
             <button type="submit">Cadastrar</button> 
             <button><a href="listaDeClientes.php">Lista de Clientes</a></button>
+            
         </p>
     </form>
 </body>
