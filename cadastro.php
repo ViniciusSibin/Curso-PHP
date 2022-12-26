98389bc0ec48f0fc7285af09dcec52baca251dfe
vinicius.sibin <vinisibim@gmail.com>
2022-12-26 17:01:38 -0300

    Alterado o formulario para envio de imagem de perfil

--- cadastro.php
+++ cadastro.php
@@ -12,7 +12,7 @@
     <title>Cadastrar Clientes</title>
 </head>
 <body>
-    <form method="POST" action="">
+    <form enctype="multipart/form-data" method="POST" action="">
         <h1>Cadastrar Clientes</h1>
         <p>Todos os campos com * são obrigatórios</p>
         <p>
@@ -39,64 +39,77 @@
             <label>Confirmar Senha:</label>
             <input value="<?php if(isset($_POST['confirmaSenha'])) echo $_POST['confirmaSenha'] ?>" type="password" name="confirmaSenha" placeholder="Ex: @1234Senha"> *
         </p>
+        <p>
+            <label>Foto de Perfil:</label>
+            <input name="fotoPerfil" type="file">
+        </p>
        
         <?php 
 
-if(count($_POST)>0){
-    $erro = false;
+        if(count($_POST)>0){
+            $erro = false;
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $nascimento = $_POST['nascimento'];
-    $senha = $_POST['senha'];
-    $confirmaSenha = $_POST['confirmaSenha'];
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $telefone = $_POST['telefone'];
+            $nascimento = $_POST['nascimento'];
+            $senha = $_POST['senha'];
+            $confirmaSenha = $_POST['confirmaSenha'];
+           
+            if(empty($nome)){
+                $erro = "Preencha o campo nome!";
+            } else if((strlen($nome)<6) || (strlen($nome)>60)){
+                $erro = "O nome deve ter entre 6 e 60 caracteres!";
+            } else if(substr($nome, 0, 1) == " "){
+                $erro = "O nome não deve iniciar com espaço";
+            }
 
+            if(empty($email)){
+                $erro = "Preencha o campo E-mail!";
+            } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $erro = "O E-mail deve ser preenchido no padrão: exemplo@gmail.com";
+            }
 
-    if(empty($nome)){
-        $erro = "Preencha o campo nome!";
-    } else if((strlen($nome)<6) || (strlen($nome)>60)){
-        $erro = "O nome deve ter entre 6 e 60 caracteres!";
-    } else if(substr($nome, 0, 1) == " "){
-        $erro = "O nome não deve iniciar com espaço";
-    }
+            if(!empty($telefone)){
+                $telefone = limpar_texto($telefone);
+                if(strlen($telefone) != 11){
+                    $erro = "O telefone foi preenchido incorretamente!";
+                }
+            }
 
-    if(empty($email)){
-        $erro = "Preencha o campo E-mail!";
-    } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $erro = "O E-mail deve ser preenchido no padrão: exemplo@gmail.com";
-    }
+            if(empty($nascimento)){
+                $erro = "Preencha o campo de data de nascimento!";
+            } else if (strlen($nascimento = implode('-', array_reverse(explode('/',$nascimento)))) != 10){
+                $erro = "Data de Nascimento está incorreta!";
+            }
 
-    if(!empty($telefone)){
-        $telefone = limpar_texto($telefone);
-        if(strlen($telefone) != 11){
-            $erro = "O telefone foi preenchido incorretamente!";
-        }
-    }
+            if(empty($senha)){
+                $erro = "Preencha o campo Senha";
+            } else if(strlen($senha)<6 || strlen($senha)>20){
+                $erro = "A senha deve conter entre 6 e 20 caracteres.";
+            } else if(substr($senha, 0, 1) == " "){
+                $erro = "A senha não deve iniciar com espaço";
+            }
 
-    if(empty($nascimento)){
-        $erro = "Preencha o campo de data de nascimento!";
-    } else if (strlen($nascimento = implode('-', array_reverse(explode('/',$nascimento)))) != 10){
-        $erro = "Data de Nascimento está incorreta!";
-    }
+            if($confirmaSenha != $senha){
+                $erro = "A confirmação da senha deve ser igual a senha digitada!";
+            }
 
-    if(empty($senha)){
-        $erro = "Preencha o campo Senha";
-    } else if(strlen($senha)<6 || strlen($senha)>20){
-        $erro = "A senha deve conter entre 6 e 20 caracteres.";
-    } else if(substr($senha, 0, 1) == " "){
-        $erro = "A senha não deve iniciar com espaço";
-    }
+            $path = "";
+            if(isset($_FILES['fotoPerfil'])){
+                $arq = $_FILES['fotoPerfil'];
+                $path = uploadArquivo ($arq['error'], $arq['size'], $arq['name'], $arq['tmp_name'], "arquivos/fotoPerfil/");
+                if($path == false){
+                    $erro = "Falha ao enviar arquivo. Tente novamente";
+                }
+            }
 
-    if($confirmaSenha != $senha){
-        $erro = "A confirmação da senha deve ser igual a senha digitada!";
-    }
             if($erro){
                 echo "<p style='color: red;'><b>ERRO: $erro</b></p>";
             } else {
                 $senha = password_hash($senha, PASSWORD_DEFAULT);
                 
-                $sql = "INSERT INTO clientes (nome, email, senha, telefone, nascimento, dataCadastro) VALUES ('$nome', '$email', '$senha', '$telefone', '$nascimento', NOW())";
+                $sql = "INSERT INTO clientes (nome, email, senha, telefone, nascimento, fotoPerfil, dataCadastro) VALUES ('$nome', '$email', '$senha', '$telefone', '$nascimento', '$path', NOW())";
                 $sqlQuery = $mysqli->query($sql) or die($mysqli->error);
     
                 $mensagem = "<h1>Parabéns $nome!</h1><p><br>Sua conta no site ... foi criada com sucesso!</p><p><b>Usuário:</b> $email <br> <b>Senha:</b> $confirmaSenha</p><p>Para realizar o login no sistema acesse <b><a href=\"http://localhost/CURSO-PHP/\">Nome do site</a></b></p>";
